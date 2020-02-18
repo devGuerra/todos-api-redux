c6a74d15da86ef316cf5742d71d4506a1b84fdbb
Roberto Guerra <roberto.santa@semparar.com.br>
2020-02-18 16:14:11 -0300

    Updates

---
+++ precache-manifest.2de99ac99db0084b8c5c68acac3fe462.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009521ada85621fb2f9395d4a851a8f4",
+    "url": "/todos-react-redux/index.html"
+  },
+  {
+    "revision": "b308335bf9e57c45434c",
+    "url": "/todos-react-redux/static/js/2.d2a27c0f.chunk.js"
+  },
+  {
+    "revision": "124f3c619ae81e2c2af3",
+    "url": "/todos-react-redux/static/js/main.bb81475f.chunk.js"
+  },
+  {
+    "revision": "ff0322708b202caafd84",
+    "url": "/todos-react-redux/static/js/runtime-main.6708533a.js"
+  }
+]);
